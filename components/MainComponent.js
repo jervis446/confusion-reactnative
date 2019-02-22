1ffc2486b349d62210577eb725b0f28522688812
jervis446 <kumar.adarshluv99@gmail.com>
2019-02-23 03:27:59 +0530

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,19 +1,83 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
+import Dishdetail from './DishdetailComponent';
+import { View, Platform } from 'react-native';
 import { DISHES } from '../shared/dishes';
+import Home from './HomeComponent';
+
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
+
+const HomeNavigator = createStackNavigator({
+  Home: { screen: Home }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff"  
+  })
+});
+
+const MainNavigator = createDrawerNavigator({
+  Home: 
+    { screen: HomeNavigator,
+      navigationOptions: {
+        title: 'Home',
+        drawerLabel: 'Home'
+      }
+    },
+  Menu: 
+    { screen: MenuNavigator,
+      navigationOptions: {
+        title: 'Menu',
+        drawerLabel: 'Menu'
+      }, 
+    }
+}, {
+drawerBackgroundColor: '#D1C4E9'
+});
+
+const MenuNavigator = createStackNavigator({
+  Menu: { screen: Menu },
+  Dishdetail: { screen: Dishdetail }
+},
+{
+  initialRouteName: 'Menu',
+  navigationOptions: {
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+          color: "#fff"            
+      }
+  }
+}
+);
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
+      selectedDish: null
     };
   }
+
+  onDishSelect(dishId) {
+    this.setState({selectedDish: dishId})
+}
 
   render() {
  
     return (
-        <Menu dishes={this.state.dishes} />
+      <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+      <MainNavigator />
+  </View>
     );
   }
 }
