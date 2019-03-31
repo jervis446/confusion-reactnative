4b30b2f6099ffc2d172cbf84fcb3b11a2db41b88
jervis446 <kumar.adarshluv99@gmail.com>
2019-03-31 11:07:39 +0530

    Redux Adding Favorites

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -5,12 +5,20 @@ import { COMMENT } from '../shared/comments';
 import { Card, Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { postFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
-      leaders: state.leaders
+      dishes: state.dishes,
+      comments: state.comments,
+      favorites: state.favorites
     }
   }
+
+const mapDispatchToProps = dispatch => ({
+    postFavorite: (dishId) => dispatch(postFavorite(dishId))
+})
+
 
 function RenderDish(props) {
 
@@ -68,21 +76,12 @@ function RenderComments(props) {
 
 class DishDetail extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            dishes: DISHES,
-            comments: COMMENTS,
-            favorite: []
-        };
-    }
-
     static navigationOptions = {
         title: 'Dish Details'
     };
 
-    markFavorite(dishId){
-        this.setState({favorite: this.state.favorite.concat(dishId)});
+    markFavorite(dishId) {
+        this.props.postFavorite(dishId);
     }
 
     render() {
@@ -90,7 +89,7 @@ class DishDetail extends Component {
         return(
             <ScrollView>
                 <RenderDish dish={this.props.dishes.dishes[+dishId]}
-                    favorite={this.state.favorites.some(el => el === dishId)}
+                    favorite={this.props.favorites.some(el => el === dishId)}
                     onPress={() => this.markFavorite(dishId)} 
                     />
                 <RenderComments comments={this.props.comments.comments.filter((comment) => comment.dishId === dishId)} />
@@ -100,4 +99,4 @@ class DishDetail extends Component {
     }
 }
 
-export default connect(mapStateToProps)(DishDetail);
+export default connect(mapStateToProps, mapDispatchToProps)(DishDetail);
